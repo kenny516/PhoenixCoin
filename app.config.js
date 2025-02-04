0fc5663b63f56cd14fe818333283ac52a2c4003d
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-02-04 23:45:20 +0300

    config eas

--- app.config.js
+++ app.config.js
@@ -2,7 +2,49 @@ import "dotenv/config";
 
 export default {
     expo: {
+        name: "phoenix-coin",
+        slug: "phoenix-coin",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/images/logo.png",
+        scheme: "phoenixcoin",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
+        ios: {
+            supportsTablet: true
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/images/adaptive-icon.png",
+                backgroundColor: "#ffffff"
+            },
+            package: "com.andriantsirafychankenny.phoenixcoin"
+        },
+        web: {
+            bundler: "metro",
+            output: "static",
+            favicon: "./assets/images/favicon.png"
+        },
+        plugins: [
+            "expo-router",
+            [
+                "expo-splash-screen",
+                {
+                    "image": "./assets/images/splash-icon.png",
+                    "imageWidth": 200,
+                    "resizeMode": "contain",
+                    "backgroundColor": "#ffffff"
+                }
+            ]
+        ],
+        experiments: {
+            typedRoutes: true
+        },
+        owner: "andriantsirafychankenny",
         extra: {
+            eas: {
+                projectId: "238b62bb-a67f-4735-bd3b-a534b1bdd8ca"
+            },
             firebaseApiKey: process.env.FIREBASE_API_KEY,
             firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
             firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
@@ -14,6 +56,6 @@ export default {
             imageKitPublicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
             imageKitPrivateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
             imageKitUrlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
-        },
-    },
+        }
+    }
 };
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore'; // Importer Firestore
 import { getDatabase } from 'firebase/database'; // Importer Realtime Database
-import { getAuth } from 'firebase/auth'; // Importer Auth
 import Constants from "expo-constants";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,9 +23,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
 const db = getFirestore(app);
-const auth = getAuth(app);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
 
-export { database, db, app, analytics, auth };
+export { database, db, app, auth };
